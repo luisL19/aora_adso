705a3609bd69d68e01122b49c48f69dae459ff04
Aprendiz <Aprendiz@BOGDFPCGMF1038.FormacionSena.RED>
2024-08-03 11:06:21 -0500

    Se agregan estilos de fuentes y colores-Video 3

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,77 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import { Slot, Stack } from "expo-router";
 
-const Header = () => {
-  return (
-    <View style={styles.header}>
-      <Text style={styles.headerText}>Header</Text>
-    </View>
-  );
-};
-
-const Footer = () => {
-  return (
-    <View style={styles.footer}>
-      <Text style={styles.footerText}>Footer</Text>
-    </View>
-  );
-};
-
 const RootLayout = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Header />
-      <Text>Luis Alejandro Lugo</Text>
-      <View style={styles.content}>
-        <Image
-          source={require('../assets/images/react-logo.png')}
-          style={styles.image}
-        />
         <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="index" 
+          options={{ headerShown: false }} />
         </Stack>
-      </View>
-      <Footer />
-    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
-  header: {
-    padding: 16,
-    backgroundColor: '#f8f8f8',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  content: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 200, 
-    height: 200,
-    marginBottom: 20, 
-  },
-  footer: {
-    padding: 16,
-    backgroundColor: '#f8f8f8',
-    borderTopWidth: 1,
-    borderTopColor: '#ddd',
-  },
-  footerText: {
-    fontSize: 16,
-    textAlign: 'center',
-  },
-});
 
 export default RootLayout;

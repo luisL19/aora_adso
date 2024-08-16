a95eab099b45e06cb6850b839f776ac991212af6
DanielZ1232 <dzuniga873@gmail.com>
2024-08-15 22:27:31 -0500

    avanzado hasta 1:29

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,9 +30,15 @@ const RootLayout = () => {
         <Stack>
           <Stack.Screen name="index" 
           options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" 
+          options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" 
+          options={{ headerShown: false }} />
+          <Stack.Screen name="/search/[query]" 
+          options={{ headerShown: false }} />
         </Stack>
   ); 
 };
 
 
-export default RootLayout;
+export default RootLayout;
